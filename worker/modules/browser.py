7e7e2edd8e207914fb2445420dd20dedae206730
Marco Dello Iacovo <marco.delloiacovo99@gmail.com>
2024-10-13 15:09:49 +0200

    async_worker_wip

--- worker/modules/browser.py
+++ worker/modules/browser.py
@@ -1,39 +1,37 @@
-from modules.encoders import RequestEncoder, ResponseEncoder
-from modules import requestMonitor
-from nodriver import start, cdp, loop
-import requests
+from nodriver import start, cdp
+from nodriver.core.browser import Browser
 import pymongo
-import asyncio
-import yaml
 import uuid
-import json
+
+import pymongo.synchronous
+import pymongo.synchronous.collection
+import pymongo.synchronous.database
+
+from model import model
 
 
-class Browser():
-    def __init__(self, config):
-        self.mongo_client = pymongo.MongoClient(f'mongodb://{config["mongodb"]["username"]}:{config["mongodb"]["password"]}@{config["mongodb"]["host"]}:{config["mongodb"]["port"]}/', uuidRepresentation='standard')  
-        self.mongo_db = self.mongo_client[config["mongodb"]["database"]]   
-        self.mongo_collection = self.mongo_db[config["mongodb"]["collection"]]
-        self.proxy = config["proxy"]['url']
+class WorkerBrowser:
+    def __init__(self, config: model.Config) -> None:
+        self.config: model.Config = config
+        self.mongo_client = pymongo.MongoClient(self.config.mongodb.get_connection_url(), uuidRepresentation="standard")
+        self.mongo_db: pymongo.synchronous.database.Database = self.mongo_client[self.config.mongodb.database]
+        self.mongo_collection: pymongo.synchronous.collection.Collection = self.mongo_db[self.config.mongodb.collection]
         self.scan_id = uuid.uuid4()
-        self.requests = {
-            'scan_id': self.scan_id,
-            'requests': [],
-            'hashes': []
-        }
+        self.requests = {"scan_id": self.scan_id, "requests": [], "hashes": []}
         loop().run_until_complete(self.start_browser())
-    
-    async def start_browser(self):
-        self.browser = await start(
-            browser_args=[f"--proxy-server={self.proxy}", "--ignore-certificate-errors", '--test-type'], 
+
+    async def start_browser(self) -> None:
+        self.browser: Browser = await start(
+            browser_args=self.config.browser.execution_args,
+            browser_executable_path=self.config.browser.executable_path,
         )
-        
+
     def load(self, url):
-        if not url.startswith('http'):
-            url = 'https://' + url
-        
+        if not url.startswith("http"):
+            url = "https://" + url
+
         loop().run_until_complete(self.main(url))
-    
+
     async def main(self, url):
         self.main_tab = self.browser.main_tab
         await self.main_tab.send(cdp.network.set_cache_disabled(True))
@@ -42,28 +40,25 @@ class Browser():
         page = await self.browser.get(url)
         retries = 0
         while retries < 7:
-            if sum(1 for dictionary in self.requests['requests'] if (len(dictionary) == 1) or (dictionary['request']['redirect_response'] is None) ) > 0:
+            if sum(1 for dictionary in self.requests["requests"] if (len(dictionary) == 1) or (dictionary["request"]["redirect_response"] is None)) > 0:
                 await page.wait(t=2)
                 retries = retries + 1
         await page
         self.mongo_collection.insert_one(self.requests)
         self.browser.stop()
-        
-    
-    
+
     async def receive_handler(self, event: cdp.network.ResponseReceived):
         response = ResponseEncoder(event)
-        if not response['response']['url'].startswith('chrome') and response['response'].get('remoteIPAddress', False):
-            res = next((req for req in self.requests['requests'] if req['request']['request_id'] == response['request_id']), None)
+        if not response["response"]["url"].startswith("chrome") and response["response"].get("remoteIPAddress", False):
+            res = next((req for req in self.requests["requests"] if req["request"]["request_id"] == response["request_id"]), None)
             if res:
-                index = self.requests['requests'].index(res)
-                self.requests['requests'][index]['response'] = response
-            if response['response'].get('headers', False) and response['response']['headers'].get('pf_sha256', False):
-                self.requests['hashes'].append(response['response']['headers']['pf_sha256'])
-
+                index = self.requests["requests"].index(res)
+                self.requests["requests"][index]["response"] = response
+            if response["response"].get("headers", False) and response["response"]["headers"].get("pf_sha256", False):
+                self.requests["hashes"].append(response["response"]["headers"]["pf_sha256"])
 
     async def send_handler(self, event: cdp.network.RequestWillBeSent):
         request = RequestEncoder(event)
-        initiator = request['initiator'].get('url', '') if request.get('initiator', False) else ''
-        if not request['request']['url'].startswith('chrome') and not initiator.startswith('chrome'):
-            self.requests['requests'].append({'request': request})
+        initiator = request["initiator"].get("url", "") if request.get("initiator", False) else ""
+        if not request["request"]["url"].startswith("chrome") and not initiator.startswith("chrome"):
+            self.requests["requests"].append({"request": request})
--- worker/worker.py
+++ worker/worker.py
@@ -1,30 +1,47 @@
-from modules.browser import Browser
+import asyncio
+from aio_pika.abc import AbstractChannel, AbstractQueue, AbstractRobustConnection
+import concurrent
+from model import model
+from modules.browser import WorkerBrowser
+import aio_pika
 import yaml
-import pika
-
-# Load config
-with open('config/config.yaml', 'r') as f_obj:
-    config = yaml.load(f_obj, Loader=yaml.SafeLoader)
 
 
-def consume():
-    credentials = pika.PlainCredentials(username=config['rabbitmq']['username'], password=config['rabbitmq']['password'])
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', credentials=credentials))
-    channel = connection.channel()
-    channel.queue_declare(queue='pf_urlqueue')
-    
+class Consumer:
+    def __init__(self, config: model.Config) -> None:
+        self.config: model.Config = config
+        self.browser = WorkerBrowser(config)
 
-    def callback(ch, method, properties, body):
-        browser = Browser(config)
-        url = body.decode()
-        browser.load(url)
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+    async def start_browser(self) -> None:
+        await self.browser.start_browser()
 
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue='pf_urlqueue', on_message_callback=callback)
+    async def _on_message(self, message: aio_pika.IncomingMessage) -> None:
+        url: str = message.body.decode()
+        if not url.startswith("http"):
+            url = "https://" + url
+        await self.browser.main(url)
+        await message.ack()
 
-    print("Waiting for messages. To exit press CTRL+C")
-    channel.start_consuming()
+    async def main(self) -> None:
+        connection: AbstractRobustConnection = await aio_pika.connect_robust(url=self.config.rabbitmq.get_connection_url(), no_ack=True)
+        channel: AbstractChannel = await connection.channel()
+        await channel.set_qos(prefetch_count=self.config.browser.max_tabs)
+        queue: AbstractQueue = await channel.declare_queue(name=self.config.rabbitmq.url_queue, durable=False)
+        await queue.consume(self._on_message)
 
-if __name__ == '__main__':
-    consume()
+
+if __name__ == "__main__":
+    # Load config
+    with open("config/config.yaml", "r") as f_obj:
+        config: model.Config = yaml.load(f_obj, Loader=yaml.SafeLoader)
+
+    consumer = Consumer(config)
+
+    loop: asyncio.AbstractEventLoop = asyncio.new_event_loop()
+
+    with concurrent.futures.ProcessPoolExecutor() as pool:
+        # Run in a different process using run_in_executor
+        loop.run_in_executor(pool, consumer.start_browser())
+
+    loop.create_task(consumer.main())
+    loop.run_forever()
